cda005bd1a1c6ea8fef07756105fed3a27d3d7ac
dianaartiom <diana.artiom24@gmail.com>
2016-06-22 18:55:07 +0300

    Made a relation between activities

--- app/src/main/java/com/example/winify/cvsi/ListItemsActivity.java
+++ app/src/main/java/com/example/winify/cvsi/ListItemsActivity.java
@@ -1,11 +1,25 @@
 package com.example.winify.cvsi;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListItemsActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+
     // Added by Cristi
     TabLayout tabLayout;
     ViewPager viewPager;
@@ -15,6 +29,9 @@ public class ListItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+
+        Intent activityThatCalled = getIntent();
+        initializeToolbar();
         init();
     }
 
@@ -28,5 +45,72 @@ public class ListItemsActivity extends AppCompatActivity {
         viewPagerAdapter.addFragments(new LoanFragment(),"Loan");
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void initNavigationDrawer() {
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    checkItemSelected(item);
+                    return true;
+                }
+            });
+
+            onActionBarDrawerToggled(navigationView);
+        }
+
+    }
+
+    public void onActionBarDrawerToggled(NavigationView navigationView) {
+        View header = navigationView.getHeaderView(0);
+        TextView tv_email = (TextView)header.findViewById(R.id.tv_email);
+        tv_email.setText("raj.amalw@learn2crack.com");
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View v){
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    public void checkItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.home:
+                Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.trash:
+                Toast.makeText(getApplicationContext(), "Trash", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                finish();
+        }
+    }
+
+    //TODO - Delete this function
+
+    public void initializeToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
---
+++ app/src/main/java/com/example/winify/cvsi/WelcomeActivity.java
@@ -0,0 +1,38 @@
+package com.example.winify.cvsi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private static Button sloginButton;
+    private static Button sItemsListButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        sloginButton = (Button) findViewById(R.id.go_to_login_button);
+        sItemsListButton = (Button) findViewById(R.id.go_to_list_view_button);
+    }
+
+    public void onSLoginButtonClicked(View view) {
+
+        Intent getLoginIntent = new Intent(this, LoginActivity.class) ;
+        final int result = 1;
+
+        getLoginIntent.putExtra("callingActivity", "WelcomeActivity");
+        startActivityForResult(getLoginIntent, result);
+    }
+
+    public void onSItemsListButtonClicked(View view) {
+        Intent getLoginIntent = new Intent(this, ListItemsActivity.class) ;
+        final int result = 1;
+
+        getLoginIntent.putExtra("callingActivity", "ListItemsActivity");
+        startActivityForResult(getLoginIntent, result);
+    }
+}
